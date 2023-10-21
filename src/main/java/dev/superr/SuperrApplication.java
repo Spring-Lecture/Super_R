c654a6391442c5c58f10ccfb4300147d0787fc3d
jun <alap_u@naver.com>
2023-10-21 16:21:16 +0900

    셋팅

---
+++ src/main/java/dev/superr/SuperrApplication.java
@@ -0,0 +1,13 @@
+package dev.superr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/superr/SuperrApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.superr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
