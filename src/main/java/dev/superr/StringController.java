c9053c000617bf150c42b1c834b30bb961f3368c
jun <alap_u@naver.com>
2023-10-21 16:32:40 +0900

    셋팅

---
+++ src/main/java/dev/superr/StringController.java
@@ -0,0 +1,21 @@
+package dev.superr;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StringController {
+    @Value("${String.hello}")
+    private final String hello;
+
+    public StringController(@Value("${String.hello}") String hello) {
+        this.hello = hello;
+    }
+
+    @GetMapping("/api/hello")
+    public String getHello() {
+        return hello;
+    }
+
+}
