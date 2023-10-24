c45005f4f1bbb598f394def7c375f966fb973c0e
jun <alap_u@naver.com>
2023-10-25 00:36:33 +0900

    테스트

--- src/main/java/dev/superr/StringController.java
+++ src/main/java/dev/superr/StringController.java
@@ -1,10 +1,12 @@
 package dev.superr;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class StringController {
     @Value("${String.hello}")
     private final String hello;
@@ -25,6 +27,7 @@ public class StringController {
 
     @GetMapping("/health")
     public String getHello3() {
+        log.info("health");
         return "OK2";
     }
 
